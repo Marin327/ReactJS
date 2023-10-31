bc8ae1da8ba35bc90a60f606807953182288d715
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2023-10-31 19:28:29 +0200

    Add files via upload

---
+++ create-react-app-demo/src/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer> All rights reserved &copy;</footer>
+}
+
+// function Footer() {
+//     return React.createElement(
+//         'div',
+//         {},
+//         'All rigths reserved &copy'; // children
+//     );
+//}
+export default Footer;
